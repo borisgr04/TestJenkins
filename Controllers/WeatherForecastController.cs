1105eb9e3fe6d35af960f9eeebede262286c17c7
Boris Arturo González Rivera <borisgr04@gmail.com>
2020-04-20 15:42:26 -0500

    test

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -12,7 +12,7 @@ namespace EjemploWebJenkins.Controllers
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
-        {
+        { 
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
